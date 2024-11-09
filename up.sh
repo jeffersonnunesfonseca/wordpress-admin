140c7cbe52cfa61ee19528e9133af2d77b8acdd6
jenunesfonseca <jefferson.nunes@chavesnamao.com.br>
2024-11-09 12:20:48 -0300

    ajustes

--- up.sh
+++ up.sh
@@ -9,4 +9,19 @@ else
   echo "A pasta db_data já existe."
 fi
 
+# Sobe os contêineres
 docker compose up -d --build --force-recreate --remove-orphans
+
+# Nome da rede
+NETWORK_NAME="visualization-exams-compose_default"
+
+# Verifica se a rede existe
+if docker network ls | grep -q "$NETWORK_NAME"; then
+  echo "Rede $NETWORK_NAME encontrada. Conectando contêineres à rede..."
+  
+  # Conecta cada contêiner à rede
+  docker network connect "$NETWORK_NAME" wpit-db || echo "wpit-db já está conectado ou houve um erro."
+  docker network connect "$NETWORK_NAME" it-wp || echo "it-wp já está conectado ou houve um erro."
+else
+  echo "Rede $NETWORK_NAME não encontrada. Nenhuma ação adicional será tomada."
+fi
